71b1a99beec2d30b8e1fb4b702c83424add8e8dd
Brian Donaldson <nairbsd@gmail.com>
2018-04-22 13:06:08 -0500

    Added task card and began working on time saving.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { isNullOrUndefined } from 'util';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  running = false;
+  taskDescription ='Collab DevEnv';
+  currentTimeElapsed = 0.3;
+  timeStart;
+
+  startTask(): void{
+    this.running = true;
+    var storageItem = Number(localStorage.getItem(this.taskDescription+'_data'));
+    this.timeStart = Number(localStorage.getItem(this.taskDescription+'_data'));
+    console.log('timestart before check: '+this.timeStart);
+    console.log('storageItem: '+storageItem);
+    
+    if(isNullOrUndefined(this.timeStart)){
+      localStorage.setItem(this.taskDescription+'_data' , Date.now().toString());  
+    }    
+  }
+
+  stopTask(): void {
+    this.running = false;
+  }
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {MatButtonModule, MatCheckboxModule, MatCardModule, MatIconModule, MatFormFieldModule, MatInputModule, MatChipsModule, MatProgressBarModule} from '@angular/material';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations'
 
 import { AppComponent } from './app.component';
+import { TimeTrackerComponent } from './time-tracker/time-tracker.component';
+import { DatePipe } from '@angular/common';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TimeTrackerComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatButtonModule, 
+    MatCheckboxModule,
+    MatCardModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatChipsModule,
+    MatProgressBarModule,
+    DatePipe,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
